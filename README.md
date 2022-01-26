3d1172c278efbaabe2bc374861b0f09c5cd63a53
ngkotha <ngayathrik9@gmail.com>
2022-01-25 21:53:43 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-4zkiya
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-4zkiya)
---
+++ index.js
@@ -0,0 +1,14 @@
+// run `node index.js` in the terminal
+
+let age = 28;
+
+function voting(){
+  if (age >= 18){
+    console.log('elogible for voting');
+  } else {
+    console.log('not eligible for voting');
+  }
+}
+voting();
+
+console.log(`My first Node program`);

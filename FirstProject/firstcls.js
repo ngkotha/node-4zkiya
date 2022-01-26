1f7de9b0a8d759428de7236ff4492c03fe80692c
ngkotha <ngayathrik9@gmail.com>
2022-01-25 22:39:33 -0600

    1 file modified

---
+++ FirstProject/firstcls.js
@@ -0,0 +1,14 @@
+
+class Gender{
+constructor(){
+}
+
+male(){
+  console.log('person is male');
+}
+female(){
+  console.log('person is female');
+}
+}
+let gender = new Gender();
+gender.female();

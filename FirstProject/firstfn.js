4a1c980ccc8f828286d3fc8378714d24918a7f5f
ngkotha <ngayathrik9@gmail.com>
2022-01-25 22:19:19 -0600

    3 files added, 1 deleted

---
+++ FirstProject/firstfn.js
@@ -0,0 +1,12 @@
+
+let age = 28;
+
+function voting(){
+  if (age > 18){
+    console.log('eligible for voting');
+  } else {
+    console.log('not eligible for voting');
+  }
+}
+voting()
+console.log(`first function`);
---
+++ FirstProject/secfn.js
@@ -0,0 +1,17 @@
+
+let fName = 'naga';
+let lName = 'k';
+
+function nameCheck(){
+  if (fname == 'naga'){
+  console.log('person is already a member')
+} else {
+console.log('not a member')
+}
+}
+
+nameCheck();
+console.log('second function');
+
+
+
